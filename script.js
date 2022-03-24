1bf6bf7fbf26daf09980cd630bd59aec12d0949a
Aleksandr <79302838+myasoedas@users.noreply.github.com>
2022-03-24 17:32:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const resultat = 4 * 4 - 4 * 5 / 2;
-alert('Ответ: ' + resultat); 
-console.log('Ответ: ' + resultat);
+alert('Ответ: resultat = 4 * 4 - 4 * 5 / 2 = ' + resultat); 
+console.log('Ответ: resultat = 4 * 4 - 4 * 5 / 2 = ' + resultat);
