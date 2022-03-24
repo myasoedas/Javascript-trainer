235b6f659d5b71c144cd030752e73319f794895c
Aleksandr <79302838+myasoedas@users.noreply.github.com>
2022-03-24 17:18:39 +0300

    feat: сложил два числа
    результат вывел во всплывающее окно и в консоль.

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert('Привет, Мир!'); 
+const number01 = 1982;
+const number02 = 39;
+let summ = number01 + number02;
+alert('Привет, Мир! ' + summ + ' год'); 
+console.log('Привет, Мир! ' + summ + ' год');
